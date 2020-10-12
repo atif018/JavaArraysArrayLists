77cd85cd82013d802ba31db40e9e7491b940aae2
Atif Hussain <atif018@gmail.com>
2020-10-12 21:48:01 +1100

    Add files via upload

---
+++ ArraysAndArrayLists/ArraysArrayLists.java
@@ -0,0 +1,33 @@
+/** This program will show working knowledge of arrays, how to manipulate them, 
+   2D arrays and arraylists. */
+   
+import java.util.Scanner;
+
+public class ArraysArrayLists
+{
+    
+    public static void main (String[] args)
+    {
+        
+        int[] numbers = new int[10];
+        int i = 0;
+        
+        Scanner input = new Scanner(System.in);
+        
+        while (i < 10)
+        {
+            System.out.println("Please enter a number: ");
+            int value = input.nextInt();
+            numbers[i] = value;
+            i++;
+        }
+        
+        System.out.print("The numbers you entered are ");
+        
+        for (i = 0; i < 10; i++)
+        {
+            System.out.println(numbers[i] + " ");
+        }
+
+    }
+}
