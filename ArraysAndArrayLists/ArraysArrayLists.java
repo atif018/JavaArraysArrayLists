66cd99b20c5b6beb54b4389571f1e7d1d13f0208
Atif Hussain <atif018@gmail.com>
2020-10-19 22:04:03 +1100

    Add files via upload

--- ArraysAndArrayLists/ArraysArrayLists.java
+++ ArraysAndArrayLists/ArraysArrayLists.java
@@ -2,6 +2,7 @@
    2D arrays and arraylists. */
    
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ArraysArrayLists
 {
@@ -111,8 +112,73 @@ public class ArraysArrayLists
         System.out.println("The following part of the program will send the inputs stored in the numbers array to a method which will add each individual number with the following number and then return the array.");
         System.out.println("For example, if the array contains 1, 2, 3, 4, 5, then 3, 5, 7, 9 will be returned.");
         
-        int[] methodArray = new int[10];
+        int[] methodArray = new int[numbers.length - 1];
         methodArray = addNumbers(numbers);
+        
+        for (i = 0; i < methodArray.length; i++)
+        {
+            
+            if (i == methodArray.length - 2)
+            {
+                System.out.print(methodArray[i] + " ");
+            }
+            else if (i == methodArray.length - 1)
+            {
+                System.out.print("and " + methodArray[i]);
+            }
+            else 
+            {
+                System.out.print(methodArray[i] + ", ");
+            }
+            
+        }
+        
+        System.out.println();
+        
+        System.out.println("The next part will deal with 2D Arrays.");
+        System.out.println("Please enter a number for the width of the 2D array");
+        int width2dArray = input.nextInt();
+        System.out.println("Please enter a number for the height of the 2D array");
+        int height2dArray = input.nextInt();
+        System.out.println("The array will now be populated by random 1s an 0s.");
+        int[][] new2dArray = new int[width2dArray][height2dArray];
+        
+        for(i = 0; i < new2dArray[0].length; i++)
+        {
+            
+            for(j = 0; j < new2dArray.length; j++)
+            {
+                
+                double randomNumber = Math.random();
+                int roundNumber = (int) Math.round(randomNumber);
+                new2dArray[i][j] = roundNumber;
+                System.out.print(new2dArray[i][j]);
+                
+            }
+            System.out.println();
+        }
+        
+        System.out.println("The last part of this program will deal with Arraylists.");
+        System.out.println("The user will be asked for several names to be input.");
+        System.out.println("These names will be saved in an Arraylist and then retrieved using various commands.");
+        ArrayList <String> names = new ArrayList<String>();
+        
+        String addedName = "";
+        int namePosition;
+        
+        while (!addedName.equals("q") || !addedName.equals("Q"))
+        {
+            
+            System.out.println("Please enter a name: ");
+            addedName = input.next();
+            names.add(addedName);
+            
+        }
+        
+        System.out.println("Please enter a number for the position of the ArrayList where you would like to retrieve the current value: ");
+        namePosition = input.nextInt();
+        System.out.println(names.get(namePosition));
+        
     }
     
     public static int[] addNumbers(int[] array)
@@ -120,8 +186,16 @@ public class ArraysArrayLists
         
         for (int i = 0; i < array.length; i++)
         {
+            if (i < (array.length - 1))
+            {
             
-            array[i] = array[i] + array[i + 1];
+                array[i] = array[i] + array[i + 1];
+                
+            }
+            else
+            {
+                break;
+            }
         
         }
         
